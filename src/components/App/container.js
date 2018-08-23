b344554fb0c9a3d4df1fcdc9501f9c233af54b85
agger89 <agger2154@gmail.com>
2018-08-23 19:24:14 +0900

    Revert "로그인 유무 state를 새로 만든 App container에게 props로 전달"
    This reverts commit 87eefa7b76d2ea8c5d909bb27b0e72464a0905df.

--- src/components/App/container.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import App from './presenter';
-
-const Container = props => <App {...props} />;
-
-export default Container;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,15 @@
-import { connect } from 'react-redux';
-import Container from './container';
+import React, { Component } from 'react';
+import styles from './styles.scss';
+import Footer from 'components/Footer';
 
-const mapStateToProps = (state, ownProps) => {
-    const { user } = state;
-    return {
-        isLoggedIn: user.isLoggedIn
+class App extends Component {
+    render() {
+        return (
+            <div className={styles.App}>
+                <Footer />
+            </div>
+                )
     }
 }
 
-// mapStateToProps 안에 isLoggedIn: user.isLoggedIn 를 Container 컴포넌트에 prop로 전달
-export default connect(mapStateToProps)(Container);
+export default App;
