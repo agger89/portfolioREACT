bf17a1b5bf1bb3592b99ffcd41d891afed6a69b4
agger89 <agger2154@gmail.com>
2018-08-24 13:24:11 +0900

    Auth 생성

--- src/components/App/presenter.js
+++ src/components/App/presenter.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
-import styles from './styles.scss';
+import './styles.scss';
 import Footer from 'components/Footer';
+import Auth from 'components/Auth';
+
 
 const App = props => [
     // nav,
@@ -9,18 +12,22 @@ const App = props => [
     <Footer key={3} />
 ];
 
+App.propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired
+};
+
 const PrivateRoutes = props => (
     <Switch>
         <Route exact path="/" render={() => "feed"} />
         <Route exact path="/explore" render={() => "explore"} />
     </Switch>
-)
+);
 
 const PublicRoutes = props => (
     <Switch>
-        <Route exact path="/" render={() => "login"} />
+        <Route exact path="/" component={Auth} />
         <Route exact path="/forgot" render={() => "password"} />
     </Switch>
-)
+);
 
 export default App;
---
+++ src/components/Auth/index.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import Container from './container';
+
+// Add all the actions for
+// Log in
+// Sign in
+// Recover psssword
+// Check username
+// Check password
+// Check email
+
+export default connect()(Container);
