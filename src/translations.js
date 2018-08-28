16d188e5142eea4f3e51037af5ab86ae5d295ced
agger89 <agger2154@gmail.com>
2018-08-28 16:31:52 +0900

    yarn run import 번역 파일 자동 생성 / ko.po 생성 - template.pot 내용 복사 - ko.po로 붙여넣기 -  상단 코드 작성 한글 번역 작성 - package.json "import": "i18n_import --encoding=utf-8" 작성

--- src/translations.js
+++ src/translations.js
@@ -1,5 +1,16 @@
 export const translations = {
-    es: {
-        "About Us": "Sbore nosotros"
-    }
-};
+  'ko': {
+    'About Us': '정보',
+    'Support': '지원',
+    'Blog': 'Blog',
+    'Press': '홍보 센터',
+    'API': 'API',
+    'Jobs': '채용 정보',
+    'Privacy': '개인정보처리방침',
+    'Terms': '약관',
+    'Directory': '디렉터리',
+    'Language': '언어',
+  },
+  'options': {
+  },
+}
