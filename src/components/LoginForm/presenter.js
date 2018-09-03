4ff43026d2a44e782a650be3d083a5abd0dbaf9c
agger89 <agger2154@gmail.com>
2018-09-03 12:53:14 +0900

    로그인 폼 input 입력시 state 업데이트 및 handle 이벤트 발생

--- src/components/LoginForm/presenter.js
+++ src/components/LoginForm/presenter.js
@@ -1,13 +1,32 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import Ionicon from "react-ionicons";
 import formStyles from 'shared/formStyles.scss';
 
 const LoginForm = props => (
     <div className={formStyles.formComponent}>
-        <form className={formStyles.form}>
-            <input type="text" placeholder="Username" className={formStyles.textInput} />
-            <input type="password" placeholder="Password" className={formStyles.textInput} />
-            <input type="submit" value="Log In" className={formStyles.button} />
+        <form className={formStyles.form} onSubmit={props.handleSubmit}>
+            <input
+                type="text"
+                placeholder="Username"
+                className={formStyles.textInput}
+                value={props.usernameValue}
+                onChange={props.handleInputChange}
+                name="username"
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                className={formStyles.textInput}
+                value={props.passwordValue}
+                onChange={props.handleInputChange}
+                name="password"
+            />
+            <input
+                type="submit"
+                value="Log In"
+                className={formStyles.button}
+            />
         </form>
         <span className={formStyles.divider}>or</span>
         <span className={formStyles.facebookLink}>
@@ -17,5 +36,13 @@ const LoginForm = props => (
         <span className={formStyles.forgotLink}>Forgot password?</span>
     </div>
 );
+
+LoginForm.propTypes = {
+    usernameValue: PropTypes.string.isRequired,
+    passwordValue: PropTypes.string.isRequired,
+    handleInputChange: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired
+};
+
 
 export default LoginForm;
