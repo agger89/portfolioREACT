fab19e870369e80caf6290eae6a11a3801005aff
agger89 <agger2154@gmail.com>
2018-09-11 11:35:39 +0900

    댓글 입력할때 change 이벤트 발생, enter 클릭시 다음칸으로 이동 안하게

--- src/components/CommentBox/presenter.js
+++ src/components/CommentBox/presenter.js
@@ -8,12 +8,21 @@ const CommentBox = (props, context) => (
         <Textarea
             className={styles.input}
             placeholder={context.t("Add a comment...")}
+            value={props.comment}
+            onChange={props.handleInputChange}
+            onKeyPress={props.handleKeyPress}
         />
     </form>
 );
 
 CommentBox.contextTypes = {
     t: PropTypes.func.isRequired
+};
+
+CommentBox.propTypes = {
+    handleInputChange: PropTypes.func.isRequired,
+    handleKeyPress: PropTypes.func.isRequired,
+    comment: PropTypes.string.isRequired
 };
 
 export default CommentBox;
